f8e318d45c2c8029f951287cc3e9e8293575bfad
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-04-10 00:21:28 +0800

    Gosh, I legit thought I lost all my commits AGAIN. Thank God I got them back, but for now I experimented with the file function. But will stop today since I feel overwhelmed after just experiencing my laptop almost breaking because of my code. Will try again later? or tomorrow since we have class later until 9pm.

--- basic_quiz_maker.py
+++ basic_quiz_maker.py
@@ -1,6 +1,15 @@
-option_count=0
+def option_one():
+    while True:
+        question=input("Question:")
+        f.write(question)
+
+        while option_count!=4:
+            option = input("Add option:")
+            f.write(option)
+            option_count = option_count + 1
 
 #Menu
+option_count = 0
 print("BASIC QUIZ MAKER")
 print("To get started\n"
       "1:Create Quiz\n"
@@ -12,21 +21,15 @@ while True:
     try:
         if choice==1:
             f=open("quiz_maker.txt", "w")
-            while True:
-                question=input("Question:")
-                f.write(question)
+            option_one()
 
-                while option_count!=4:
-                    option=input("Add option:")
-                    f.write(option)
-                    option_count=option_count+1
-
-            ask=input("Add another question?(Y/N)")
+            ask = input("Add another question?(Y/N)")
             if ask=="Y" or ask=="y":
                 continue
-            else:
+            if ask=="N" or ask=="n":
+                f.close()
+                f.open("quiz_maker.txt")
                 break
-
 
         elif choice==2:
             print("Goodbye!")
