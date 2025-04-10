a6c3ed79ac6b5ec63506c47d72cb0e998685f545
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-04-10 08:38:46 +0800

    Another day, another attempt. I moved the whole of choice one inside the def 'option_one'. Still unsure why, but the variable 'option_count' is having some errors and not working properly which is why it's not asking for options. I wonder why.

--- basic_quiz_maker.py
+++ basic_quiz_maker.py
@@ -1,12 +1,24 @@
 def option_one():
+    option_count=0
+    f = open("quiz_maker.txt", "w")
+
     while True:
         question=input("Question:")
         f.write(question)
-
         while option_count!=4:
             option = input("Add option:")
             f.write(option)
             option_count = option_count + 1
+
+        ask = input("Add another question?(Y/N)")
+        if ask == "Y" or ask == "y":
+            continue
+        if ask == "N" or ask == "n":
+            print("Thank you for using basic quiz maker. Be sure to rename your file or else it will"
+                  "be replaced if you make another one.")
+            f.close()
+            f.read()
+            break
 
 #Menu
 option_count = 0
@@ -14,26 +26,21 @@ print("BASIC QUIZ MAKER")
 print("To get started\n"
       "1:Create Quiz\n"
       "2:Exit")
-choice=(int(input("What do you want to do? Pick 1 or 2:")))
 
 #Main Prompt
 while True:
     try:
+        choice = (int(input("\nWhat do you want to do? Pick 1 or 2:")))
         if choice==1:
-            f=open("quiz_maker.txt", "w")
+            print("\n----------\n\nYou have chosen 'CREATE QUIZ'\n")
             option_one()
 
-            ask = input("Add another question?(Y/N)")
-            if ask=="Y" or ask=="y":
-                continue
-            if ask=="N" or ask=="n":
-                f.close()
-                f.open("quiz_maker.txt")
-                break
-
         elif choice==2:
-            print("Goodbye!")
+            print("\nGoodbye!")
             break
 
-    except:
-        print("You can only pick between 1 and 2.")
+        else:
+            print("\nYou can only pick between 1 and 2.\n\n----------\n")
+
+    except ValueError:
+        print("\nYou can only pick between 1 and 2.\n\n----------\n")
