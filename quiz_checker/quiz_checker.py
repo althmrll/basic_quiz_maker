b41f3cf9c7365f36c7b824421fe845becc1d3640
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-05-06 14:17:00 +0800

    Wrote pseudocode

---
+++ quiz_checker/quiz_checker.py
@@ -0,0 +1,8 @@
+#QUIZ CHECKER
+#Ask user the quiz they want to answer (use pop-up?)
+#Print one question with choices for user to answer
+#Make user press 'submit' (or enter) button
+#After submitting, show user the correct answer
+#Add 1 to number of corrects (if correct) and the number of questions answered over the total number of questions.
+#Flash ending message, positive if passed, negative if failed. (try adding multiple ending mesages done at random)
+#Add choice wether to exit the program or answer another quiz.
