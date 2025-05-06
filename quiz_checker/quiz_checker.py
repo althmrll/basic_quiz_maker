6732246a6cc4ce963503ad336a79e3939fe81dc4
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-05-06 15:18:21 +0800

    Tried tkinter to created a button and produce a pop up so user can choose which quiz they want to answer.

--- quiz_checker/quiz_checker.py
+++ quiz_checker/quiz_checker.py
@@ -1,13 +1,30 @@
 #QUIZ CHECKER
 import tkinter as tk_module
 
-#Ask user the quiz they want to answer (use pop-up?)
 window=tk_module.Tk()
+
+#definitions
+def choose_button():
+    choose_file = choose_button.askopenfilename()
+    return choose_file
+
+
+#Ask user the quiz they want to answer (use pop-up?)
 window.title("Quiz Checker")
 window.geometry("600x400") 
 label = tk_module.Label(window, text="QUIZ CHECKER\nThis quiz checker is partnered with the Basic Quiz Creator. If you have no" \
 "quiz available, Create one now. Or you can click the button below so you can choose which quiz to answer.")
 label.pack()
+
+button = tk_module.Button(window, text="Choose file", command=choose_button)
+button.pack()
+
+choose_file = choose_button()
+if choose_file:
+    print("Selected file:",choose_file)
+else:
+    print("No file selected")
+
 window.mainloop()
 
 #Print one question with choices for user to answer
