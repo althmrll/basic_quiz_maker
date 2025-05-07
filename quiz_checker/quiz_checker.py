46a70cf971a48e631081c2eb344525c810da10c9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-05-07 18:06:25 +0800

    Used os to center text and tkinter to produce a pop-up so user can choose. Also, coded the program for after pressing enter.

--- quiz_checker/quiz_checker.py
+++ quiz_checker/quiz_checker.py
@@ -1,19 +1,35 @@
 import os
+import tkinter as tk_module
+from tkinter import filedialog
 
 #DEFINITIONS
 def center(center_text):
     screen_size=os.get_terminal_size().columns
     return center_text.center(screen_size)
 
+def start_program():
+    start=input("")
+    while True:
+        if start == "":
+            choose_file()
+        else: 
+            start=input("Press 'ENTER' to start the game.")
+
+def choose_file():
+    chosen_file = filedialog.askopenfilename()
+    if chosen_file:
+        print("hehe")
+
 #FIXED VARIABLES
 program_title="QUIZ CHECKER"
-opening_message="\n\nThis program is used so you can answer the quiz you just created. You can also use this so other people can answer your created quiz. Press 'ENTER' to choose which quiz you want to answer.\n\n"
+opening_message="\nThis program is used so you can answer the quiz you just created. You can also use this so other people can answer your created quiz. Press 'ENTER' to choose which quiz you want to answer."
 centered_title=center(program_title)
 
-#QUIZ CHECKER
+#QUIZ CHECKER (main program)
 print(centered_title)
 print(opening_message)
-#Ask user the quiz they want to answer (use pop-up?)
+start_program()
+
 #Print one question with choices for user to answer
 #Make user press 'submit' (or enter) button
 #After submitting, show user the correct answer
